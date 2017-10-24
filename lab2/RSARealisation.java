4abf9c1fc8a76edc24c51a821a7f530407b4afd0
Vadym <vadimka12@ukr.net>
2017-10-24 20:42:37 +0300

    Update1

--- lab2/RSARealisation.java
+++ lab2/RSARealisation.java
@@ -13,6 +13,7 @@ public class RSARealisation {
         A.setN(A.getP().multiply(A.getQ()));
         A.setE(BigInteger.valueOf(65537));
         A.setD(A.getE().modInverse(A.phiN()));
+        
     }
 
     public static BigInteger encryption(Abonent A){
