44d1f70d6e83304e2f49a6081d3323501b933918
drerek <adrerek@gmail.com>
2017-10-24 14:46:13 +0300

    lab2

--- lab2/RSA.java
+++ lab2/RSA.java
@@ -12,16 +12,21 @@ public class RSA {
         return a < 0 ? -a : a;
     }
 
-    static boolean isPseudoFerma(int p, int k){
-        int i=0;
-        while (i < k){
-            int x = new Random().nextInt(p-1)+1;
-            if (GCD(x,p)>1) return false;
-            if (GCD(x,p)!=1 && BigInteger.valueOf(x).modPow(BigInteger.valueOf(p-1),BigInteger.valueOf(p)).intValue() != 1){
+    static boolean isPseudoFerma(int p, int k) {
+        int i = 0;
+        if (p % 2 != 0) return false;
+        while (i < k) {
+            int x = new Random().nextInt(p - 1) + 1;
+            if (GCD(x, p) > 1) return false;
+            if (GCD(x, p) != 1 && BigInteger.valueOf(x).modPow(BigInteger.valueOf(p - 1), BigInteger.valueOf(p)).intValue() != 1) {
                 return false;
             }
             i++;
         }
         return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPseudoFerma(10,2));
     }
 }
