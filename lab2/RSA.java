eaed23b5f3b42eab6ea4393b8d3a18b7e0d1c31e
drerek <adrerek@gmail.com>
2017-10-24 14:44:01 +0300

    lab2

---
+++ lab2/RSA.java
@@ -0,0 +1,27 @@
+package lab2;
+
+import java.math.BigInteger;
+import java.util.Random;
+
+public class RSA {
+
+    private static long GCD(long a, long b)
+    {
+        while (b != 0)
+            b = a % (a = b);
+        return a < 0 ? -a : a;
+    }
+
+    static boolean isPseudoFerma(int p, int k){
+        int i=0;
+        while (i < k){
+            int x = new Random().nextInt(p-1)+1;
+            if (GCD(x,p)>1) return false;
+            if (GCD(x,p)!=1 && BigInteger.valueOf(x).modPow(BigInteger.valueOf(p-1),BigInteger.valueOf(p)).intValue() != 1){
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
+}
