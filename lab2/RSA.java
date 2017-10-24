7fbec6b790afc77c016ff40736e97c7e15b07d65
drerek <adrerek@gmail.com>
2017-10-24 17:32:21 +0300

    lab2

--- lab2/RSA.java
+++ lab2/RSA.java
@@ -5,41 +5,41 @@ import java.util.Random;
 
 public class RSA {
 
-    private static long GCD(long a, long b)
-    {
-        while (b != 0)
-            b = a % (a = b);
-        return a < 0 ? -a : a;
-    }
 
-    private static long rnd(long min, long max)
-    {
-        max -= min;
-        final double random = Math.random();
-        return Math.round((random * max) + min);
-    }
-
-    private static boolean isPseudoFerma(long p, long k) {
-        int i = 0;
-        if (p % 2 == 0) return false;
-        while (i < k) {
-            long x = rnd(2,p-1);
-            if (GCD(x, p) > 1) return false;
-            if (GCD(x, p) != 1 || BigInteger.valueOf(x).modPow(BigInteger.valueOf(p - 1), BigInteger.valueOf(p)).intValue() != 1) {
+    private static boolean isPseudoFerma(BigInteger p, BigInteger k) {
+        BigInteger i = BigInteger.ZERO;
+        if (p.mod(BigInteger.valueOf(2)).compareTo(BigInteger.ZERO) == 0) return false;
+        while (i.compareTo(k) != 0) {
+         //   System.out.println("i=" + i.intValue());
+            BigInteger x = new BigInteger(p.bitLength()-1, new Random());
+            BigInteger result = x.gcd(p);
+            if (result.compareTo(BigInteger.ONE) == 1) return false;
+            if (result.compareTo(BigInteger.ONE) == 0 && x.modPow(p.divide(BigInteger.ONE), p).compareTo(BigInteger.ONE) == 0) {
                 return false;
             }
-            i++;
+            i = i.add(BigInteger.ONE);
         }
         return true;
     }
 
-    static long pseudoFerma(long min, long max, long k){
-    long p = rnd(min,max);
-        while (!isPseudoFerma(p,k)) p=rnd(min,max);
-        return p;
+    private static BigInteger rnd(int min, int max) {
+        //includeve [2^min;2^min+max]
+        return new BigInteger(max, new Random()).multiply(BigInteger.valueOf(2).pow(min)).add(BigInteger.ONE);
+    }
+
+    static BigInteger pseudoFerma(int min, int max, BigInteger k) {
+        BigInteger p=rnd(min,max);
+        while (true){
+            if (isPseudoFerma(p, k) && p.bitLength() == 256)
+                return p;
+            p=rnd(min,max);
+        }
     }
 
     public static void main(String[] args) {
-       // System.out.println(pseudoFerma());
+
+        BigInteger p =pseudoFerma(128,128,BigInteger.valueOf(2).pow(10));
+         System.out.println(p);
+        System.out.println(p.bitLength());
     }
 }
